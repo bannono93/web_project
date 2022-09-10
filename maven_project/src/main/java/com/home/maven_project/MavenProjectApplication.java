435abd4f3500c9776dcea6e99f3aa650ceec3b10
banno <banno@bannono93>
2022-09-11 00:35:34 +0900

    First project commit test

---
+++ maven_project/src/main/java/com/home/maven_project/MavenProjectApplication.java
@@ -0,0 +1,12 @@
+package com.home.maven_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjectApplication.class, args);
+	}
+}
